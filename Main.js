5ce1bf3571b8a25d0888b76a82ea333d6a64363c
TchStud2143 <91428255+TchStud2143@users.noreply.github.com>
2021-09-26 19:46:50 +0300

    Updated Main.js

--- Main.js
+++ Main.js
@@ -1,24 +1,34 @@
 const fs = require('fs');
-const { model } = require('./dist/ClassifierModel.js');
+const { model } = require('./ClassifierModel.js');
+const { default: Tensor } = require('./Tensor.js');
 
-//var dataFileBuffer = fs.readFileSync(__dirname + '/train-images-idx3-ubyte');
-//var labelFileBuffer = fs.readFileSync(__dirname + '/train-labels-idx1-ubyte');
-//var pixelValues = [];
+var dataFileBuffer = fs.readFileSync(__dirname + '/train-images-idx3-ubyte.gz');
+var labelFileBuffer = fs.readFileSync(
+  __dirname + '/train-labels-idx1-ubyte.gz'
+);
+const pixelValues = [];
 
-// It would be nice with a checker instead of a hard coded 60000 limit here
-/*for (let image = 0; image <= 59999; image++) {
-  var pixels = [];
+for (let image = 0; image <= 59999; image++) {
+  let pixels = [];
 
-  for (var x = 0; x <= 27; x++) {
-    for (var y = 0; y <= 27; y++) {
+  for (let x = 0; x <= 27; x++) {
+    for (let y = 0; y <= 27; y++) {
       pixels.push(dataFileBuffer[image * 28 * 28 + (x + y * 28) + 15]);
     }
   }
 
-  var imageData = {};
+  let imageData = {};
   imageData[JSON.stringify(labelFileBuffer[image + 8])] = pixels;
 
   pixelValues.push(imageData);
-}*/
+}
 
-console.log(model);
+for (const imageData of pixelValues) {
+  for (const label in imageData) {
+    const inputs = new Tensor(28, 28, 1);
+    inputs.out = imageData[label];
+    model.train(inputs, label);
+  }
+}
+
+// TODO: Accept user input
